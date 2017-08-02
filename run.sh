ff55e2184835aff856d693e52a6e1de57c381d25
superdavidxp <daweimu2012@gmail.com>
2017-08-02 12:18:16 -0700

    rename the transpose to vecadd

--- run.sh
+++ run.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-./cupti_transpose 0 achieved_occupancy
+./cupti_vecadd 0 achieved_occupancy
 echo -------------------------------------------------------
-./cupti_transpose 0 branch_efficiency
+./cupti_vecadd 0 branch_efficiency
 echo -------------------------------------------------------
-./cupti_transpose 0 dram_read_throughput
+./cupti_vecadd 0 dram_read_throughput
 echo -------------------------------------------------------
-./cupti_transpose 0 dram_write_throughput
+./cupti_vecadd 0 dram_write_throughput
 echo -------------------------------------------------------
-./cupti_transpose 0 flop_count_sp
+./cupti_vecadd 0 flop_count_sp
 echo -------------------------------------------------------
-./cupti_transpose 0 flop_sp_efficiency
+./cupti_vecadd 0 flop_sp_efficiency
 echo -------------------------------------------------------
-./cupti_transpose 0 gld_efficiency
+./cupti_vecadd 0 gld_efficiency
 echo -------------------------------------------------------
-./cupti_transpose 0 gld_throughput
+./cupti_vecadd 0 gld_throughput
 echo -------------------------------------------------------
-./cupti_transpose 0 gst_throughput
+./cupti_vecadd 0 gst_throughput
 echo -------------------------------------------------------
-./cupti_transpose 0 sm_activity
+./cupti_vecadd 0 sm_activity
 echo -------------------------------------------------------
-./cupti_transpose 0 l2_utilization
+./cupti_vecadd 0 l2_utilization
 echo -------------------------------------------------------
-./cupti_transpose 0 local_hit_rate
+./cupti_vecadd 0 local_hit_rate
 echo -------------------------------------------------------
-./cupti_transpose 0 tex_cache_hit_rate
+./cupti_vecadd 0 tex_cache_hit_rate
 echo -------------------------------------------------------
-./cupti_transpose 0 shared_efficiency
+./cupti_vecadd 0 shared_efficiency
 echo -------------------------------------------------------
-./cupti_transpose 0 sm_efficiency
+./cupti_vecadd 0 sm_efficiency
